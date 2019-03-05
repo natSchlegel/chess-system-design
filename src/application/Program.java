532fca14b23e889ef0d50ba96d60d3aa6a706470
natas <natas@192.168.5.109>
2019-03-04 22:55:59 -0300

    Testando o GitHub no NetBeans.

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,11 +3,12 @@ package application;
 
 import boardgame.Board;
 
-
 public class Program {
 
     public static void main(String[] args) {
         Board board = new Board(8,8);
+     
+               System.out.println("Testando GitHub no Netbeans");
     }
     
 }
